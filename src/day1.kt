cb8c22e5b1cc736c41cb542ac8657c9eea6765b9
xinruiy <xinruiy@google.com>
2021-12-03 09:26:18 -0800

    Day 1 of AOC

---
+++ src/day1.kt
@@ -0,0 +1,21 @@
+import java.io.File
+
+fun countIncrease(measurements: List<Int>): Int {
+  var last = Integer.MAX_VALUE
+  return measurements.map {
+    val tmp = last
+    last = it
+    if (last > tmp) 1 else 0
+  }.sum()
+}
+
+fun getWindowList(measurements: List<Int>): List<Int> {
+  return measurements.windowed(3).map { it.sum() }
+}
+
+fun main(args: Array<String>) {
+  var data = mutableListOf<Int>()
+  File("src/data").forEachLine { data.add(it.toInt()) }
+  println(countIncrease(data))
+  println(countIncrease(getWindowList(data)))
+}
